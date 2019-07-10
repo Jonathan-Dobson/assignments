0a11a9eb078294190965332af35d587533e5747e
fromjdobson <fromjdobson@gmail.com>
2019-07-10 10:55:38 -0600

    exercise 1 hello.js

---
+++ exercises/js01/hello.js
@@ -0,0 +1,27 @@
+// Tasks Level One
+
+// call the function
+// change this function expression to a function declaration
+// Tasks Level Two
+
+// Give the function a name parameter
+// Pass the function call a string (preferably a name) as an argument.
+// Return a cool string, like: "Hello Fred!"!
+// Tasks Level Three
+
+// Give the function a age parameter
+// Pass the function call a string (preferably a age) as an argument.
+// Return a cool string, like: "Hello Fred, how do you like being 27?"
+
+
+// var sayHello = function(){
+//     console.log("hello class")
+// }
+
+
+function sayHello(name,age){
+    return (`Hello ${name}. How do you like being ${age}`)
+}
+
+
+console.log(sayHello("Jonathan",37))
