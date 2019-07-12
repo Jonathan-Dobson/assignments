960be882c502c0d2d2136d0ae6f3f1ddc0b693a3
fromjdobson <fromjdobson@gmail.com>
2019-07-12 16:39:04 -0600

    string methods practice

--- exercises/functions/string.js
+++ exercises/functions/string.js
@@ -10,6 +10,60 @@
 //     .join(' ')
 
 
-let capitalize = s => s.split(' ').map(e=>e[0].toUpperCase()+e.slice(1)).join(' ')
+// let capitalize = s => s.split(' ').map(e=>e[0].toUpperCase()+e.slice(1)).join(' ')
 
 // console.log(capitalize("hey friends! practice practice practice!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findMiddleIndex(str){
+//     return Math.floor(str.length / 2)
+// }
+
+// console.log(findMiddleIndex("hello"))
+
+
+
+// function capilizeAndLowercase(str){
+//     return Math.floor(str.length / 2)
+
+// }
+
+
+
+
+// console.log(
+//     capilizeAndLowercase("Hello") // => "HEllo"
+// )
+// console.log(
+//     capilizeAndLowercase("Hello World") // => "HELLO world"
+// )
+
+
+
+
+
+
+
+
+
+
+function returnFirstHalf(s){
+    let h = s.length/2
+    return s.slice(0,h).toUpperCase() + s.slice(h).toLowerCase()
+}
+
+
+console.log(returnFirstHalf("Hello")) 
+console.log(returnFirstHalf("Hello World")) // => "Hello"
