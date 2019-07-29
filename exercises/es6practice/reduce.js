cfe21b149f2e9116929607da260ef7c3b9a3b6e9
fromjdobson <fromjdobson@gmail.com>
2019-07-28 20:19:30 -0600

    djjs2

--- exercises/es6practice/reduce.js
+++ exercises/es6practice/reduce.js
@@ -87,45 +87,49 @@
 
 
 
-var voters = [
-    {name:'Bob' , age: 30, voted: true},
-    {name:'Jake' , age: 32, voted: true},
-    {name:'Kate' , age: 25, voted: false},
-    {name:'Sam' , age: 20, voted: false},
-    {name:'Phil' , age: 21, voted: true},
-    {name:'Ed' , age:55, voted:true},
-    {name:'Tami' , age: 54, voted:true},
-    {name: 'Mary', age: 31, voted: false},
-    {name: 'Becky', age: 43, voted: false},
-    {name: 'Joey', age: 41, voted: true},
-    {name: 'Jeff', age: 30, voted: true},
-    {name: 'Zack', age: 19, voted: false}
-];
+// var voters = [
+//     {name:'Bob' , age: 30, voted: true},
+//     {name:'Jake' , age: 32, voted: true},
+//     {name:'Kate' , age: 25, voted: false},
+//     {name:'Sam' , age: 20, voted: false},
+//     {name:'Phil' , age: 21, voted: true},
+//     {name:'Ed' , age:55, voted:true},
+//     {name:'Tami' , age: 54, voted:true},
+//     {name: 'Mary', age: 31, voted: false},
+//     {name: 'Becky', age: 43, voted: false},
+//     {name: 'Joey', age: 41, voted: true},
+//     {name: 'Jeff', age: 30, voted: true},
+//     {name: 'Zack', age: 19, voted: false}
+// ];
 
-function voterResults(arr) {
-    return arr.reduce((a,b)=>{
-        if(b.age >= 18 && b.age <= 25){
-            a.youth++
-            a.youngVotes += b.voted ? 1 : 0    
-        }
-        if(b.age >= 26 && b.age <= 35){
-            a.mids++
-            a.midVotes += b.voted ? 1 : 0    
-        }
-        if(b.age >= 36 && b.age <= 55){
-            a.olds++
-            a.oldVotes += b.voted ? 1 : 0
-        }
-        return a
-    }
-    ,{
-        youngVotes: 0,
-        youth: 0,
-        midVotes: 0,
-        mids: 0,
-        oldVotes: 0,
-        olds: 0
-    })
-}
+// function voterResults(arr) {
+//     return arr.reduce((a,b)=>{
+//         if(b.age >= 18 && b.age <= 25){
+//             a.youth++
+//             a.youngVotes += b.voted ? 1 : 0    
+//         }
+//         if(b.age >= 26 && b.age <= 35){
+//             a.mids++
+//             a.midVotes += b.voted ? 1 : 0    
+//         }
+//         if(b.age >= 36 && b.age <= 55){
+//             a.olds++
+//             a.oldVotes += b.voted ? 1 : 0
+//         }
+//         return a
+//     }
+//     ,{
+//         youngVotes: 0,
+//         youth: 0,
+//         midVotes: 0,
+//         mids: 0,
+//         oldVotes: 0,
+//         olds: 0
+//     })
+// }
 
-console.log(voterResults(voters));
+// console.log(voterResults(voters));
+
+
+
+
---
+++ exercises/es6practice/sortymcsortsort.js
@@ -0,0 +1,56 @@
+const users = [
+    {
+        firstName: "Billy",
+        lastName: "Madison", 
+        age: 32
+    },
+    {
+        firstName: "Tommy",
+        lastName: "Pickles", 
+        age: 32
+    },
+    {
+        firstName: "Ray",
+        lastName: "Charles", 
+        age: 32
+    },
+    {
+        firstName: "Lady",
+        lastName: "Gaga", 
+        age: 32
+    },
+    {
+        firstName: "Elizabeth",
+        lastName: "Taylor", 
+        age: 32
+    },
+    {
+        firstName: "Led",
+        lastName: "Zeplin", 
+        age: 32
+    },
+    {
+        firstName: "Micael",
+        lastName: "Johnson", 
+        age: 32
+    },
+    {
+        firstName: "Keri",
+        lastName: "Strug", 
+        age: 32
+    },
+    {
+        firstName: "Chucky",
+        lastName: "Finster", 
+        age: 32
+    },
+    {
+        firstName: "Angelica",
+        lastName: "Pickles", 
+        age: 32
+    },
+]
+
+
+
+
