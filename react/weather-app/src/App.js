009d909481ee08ea77356ba1307405aa40dc386e
fromjdobson <fromjdobson@gmail.com>
2019-08-19 11:57:33 -0600

    made current conditions template

--- react/weather-app/src/App.js
+++ react/weather-app/src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>hello</div>
+    <div className="container">
+        <div className="summary">Rain starting later this afternoon, continuing until this evening.</div>
+        <div className="current-conditions">
+            <div className="temp">78 &deg;F</div>
+            <div className="location">in Salt Lake City</div>
+            <div className="change-location">change location...</div>
+        </div>
+        <div></div>
+        <button className="week-forecast">View 7-Day Forecast</button>      
+
     </div>
   );
 }
