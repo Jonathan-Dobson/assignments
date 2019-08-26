086d5a3c75484bc18591b77e35a0e0b6bd8c0ab9
fromjdobson <fromjdobson@gmail.com>
2019-08-25 21:35:35 -0600

    clean up version 1 files

--- react/weather-app2/src/original/MainComponents/SmallComponents/City.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { WithState } from '../../StateProvider';
-
-const City = ({city,state,country}) => 
-    <div className="location"> {city} {state}, {country}</div>
-
-export default WithState(City)
