940d4e16164fad5658524d65f751df9c519a9c23
fromjdobson <fromjdobson@gmail.com>
2019-09-30 20:04:23 -0600

    add a portfolio item weather app

--- main.js
+++ main.js
@@ -1,90 +1,123 @@
 const {log} = console
 
 const data = [
-{
-    Title: `CSS Zen Garden Project`,
-    Description: `Reproduce a PSD design with only HTML and CSS`,
-    CourseLink: `https://coursework.vschool.io/css-zen-garden/`,
-    LiveDemo: `./exercises/zenGarden/test8/test.html`,
-    keyPoints: ['HTML', 'CSS', 'PSD', "Layout", "CSS Grid"],
-    bgColorTop: `rgba(255, 205, 64, 0.3)`,
-    bgColorBottom: `rgba(73, 144, 153, 0.8)`,
-    image: `./images/zengarden.png`
-}, {    
-    Title: `Axios Todo Project`,
-    Description: `Build a frontend site that pulls a list of todos down from the API to display them, and allows the user to perform other CRUD methods on them as well.`,
-    CourseLink: `https://coursework.vschool.io/axios-todo-practice/`,
-    LiveDemo: `./projects/axios/index.html`,
-    keyPoints: ['HTML', 'CSS', 'DOM', "Event Listeners", "Forms", "API", "Axios", "XHR", "Async"],
-    bgColorTop: `rgba(255, 205, 64, 0.3)`,
-    bgColorBottom: `rgba(73, 144, 153, 0.8)`,
-    image: `https://coursework.vschool.io/content/images/2017/08/advisor-to-do-list.jpg`
-}, {
-    Title: `"Business Time" Project`,
-    Description: `Create a website for your new business that is well-styled and responsive`,
-    CourseLink: `https://coursework.vschool.io/business-time/`,
-    LiveDemo: `./projects/businessTime3/index.html`,
-    keyPoints: ['HTML', 'CSS', 'Layout', "Media Queries"],
-    bgColorTop: `rgba(73, 144, 153, 0.3)`,
-    bgColorBottom: `rgba(73, 144, 153, 0.8)`,
-    image: `./images/business-time.png`
-}, {
-    Title: `Newsies`,
-    Description: `Re-create the front page of a newspaper using block, inline and inline-block elements.`,
-    CourseLink: `http://coursework.vschool.io/block-vs-inline-newsies/`,
-    LiveDemo: `./projects/newsies/third/index.html`,
-    keyPoints: ['HTML', 'CSS', 'Layout', "Box Model"],
-    bgColorTop: `rgba(119, 153, 134, 0.3)`,
-    bgColorBottom: `rgba(119, 153, 134, 1)`,
-    image: `./images/newsies.png`
-}, {
-    Title: "Mario Pest Control",
-    Description: `Build Mario a website that keeps track of how many baddies he caught for Princess Peach so he can send her a bill.`,
-    CourseLink: `https://coursework.vschool.io/mario-pest-control/`,
-    LiveDemo: `./projects/marioPestControl/index.html`,
-    keyPoints: ['HTML', 'DOM', 'JavaScript', 'Event Listeners'],
-    bgColorTop: `rgba(255, 251, 0, 0.4)`,
-    bgColorBottom: `rgba(255, 123, 0, 1)`,
-    image: `./images/mario.png`
-}, {
-    Title: `Design a Blog`,
-    Description: `Design a responsive Blog page with a content feed`,
-    CourseLink: `http://coursework.vschool.io/design-a-blog/`,
-    LiveDemo: `./exercises/fromjon/blog/index.html`,
-    keyPoints: ['HTML', 'CSS', 'Layout', "Box Model", "Media Queries"],
-    bgColorTop: `rgba(119, 153, 134, 0.3)`,
-    bgColorBottom: `rgba(135, 71, 51, 1)`,
-    image: `./images/blog.png`
-}, {
-    Title: `CSS Flags`,
-    Description: `Build the following flags in pure HTML and CSS, no images allowed.`,
-    CourseLink: `https://coursework.vschool.io/css-flags/`,
-    LiveDemo: `./exercises/flags/index.html`,
-    keyPoints: ['HTML', 'CSS', 'Layout', "Box Model"],
-    bgColorTop: `rgba(119, 153, 134, 0.3)`,
-    bgColorBottom: `rgba(135, 71, 51, 1)`,
-    image: `./images/flags2.png`
-}, {
-    Title: `Simple Calculator`,
-    Description: `create a simple calculator that can Add, Subtract, and Multiply`,
-    CourseLink: `https://coursework.vschool.io/simple-calculator/`,
-    LiveDemo: `./exercises/simpleCalc/index.html`,
-    keyPoints: ['HTML', 'CSS', "DOM", "Event Listeners", "Forms"],
-    bgColorTop: `rgba(119, 153, 134, 0.3)`,
-    bgColorBottom: `rgba(135, 71, 51, 1)`,
-    image: `./images/simpleCalc.png`
-} 
+    {
+        Title: `React Weather App`,
+        Description: `Daily and weekly weather data from the darksky API`,
+        CourseLink: ``,
+        LiveDemo: `https://jonathans-weather-app.herokuapp.com/`,
+        keyPoints: [
+            'React', 'Axios', "REST API"
+        ],
+        bgColorTop: `rgba(255, 205, 64, 0.3)`,
+        bgColorBottom: `rgba(73, 144, 153, 0.8)`,
+        image: `./images/weather.png`
+    }, {
+        Title: `CSS Zen Garden Project`,
+        Description: `Reproduce a PSD design with only HTML and CSS`,
+        CourseLink: `https://coursework.vschool.io/css-zen-garden/`,
+        LiveDemo: `./exercises/zenGarden/test8/test.html`,
+        keyPoints: [
+            'HTML', 'CSS', 'PSD', "Layout", "CSS Grid"
+        ],
+        bgColorTop: `rgba(255, 205, 64, 0.3)`,
+        bgColorBottom: `rgba(73, 144, 153, 0.8)`,
+        image: `./images/zengarden.png`
+    }, {
+        Title: `Axios Todo Project`,
+        Description: `Build a frontend site that pulls a list of todos down from the API to display them, and allows the user to perform other CRUD methods on them as well.`,
+        CourseLink: `https://coursework.vschool.io/axios-todo-practice/`,
+        LiveDemo: `./projects/axios/index.html`,
+        keyPoints: [
+            'HTML',
+            'CSS',
+            'DOM',
+            "Event Listeners",
+            "Forms",
+            "API",
+            "Axios",
+            "XHR",
+            "Async"
+        ],
+        bgColorTop: `rgba(255, 205, 64, 0.3)`,
+        bgColorBottom: `rgba(73, 144, 153, 0.8)`,
+        image: `https://coursework.vschool.io/content/images/2017/08/advisor-to-do-list.jpg`
+    }, {
+        Title: `"Business Time" Project`,
+        Description: `Create a website for your new business that is well-styled and responsive`,
+        CourseLink: `https://coursework.vschool.io/business-time/`,
+        LiveDemo: `./projects/businessTime3/index.html`,
+        keyPoints: [
+            'HTML', 'CSS', 'Layout', "Media Queries"
+        ],
+        bgColorTop: `rgba(73, 144, 153, 0.3)`,
+        bgColorBottom: `rgba(73, 144, 153, 0.8)`,
+        image: `./images/business-time.png`
+    }, {
+        Title: `Newsies`,
+        Description: `Re-create the front page of a newspaper using block, inline and inline-block elements.`,
+        CourseLink: `http://coursework.vschool.io/block-vs-inline-newsies/`,
+        LiveDemo: `./projects/newsies/third/index.html`,
+        keyPoints: [
+            'HTML', 'CSS', 'Layout', "Box Model"
+        ],
+        bgColorTop: `rgba(119, 153, 134, 0.3)`,
+        bgColorBottom: `rgba(119, 153, 134, 1)`,
+        image: `./images/newsies.png`
+    }, {
+        Title: "Mario Pest Control",
+        Description: `Build Mario a website that keeps track of how many baddies he caught for Princess Peach so he can send her a bill.`,
+        CourseLink: `https://coursework.vschool.io/mario-pest-control/`,
+        LiveDemo: `./projects/marioPestControl/index.html`,
+        keyPoints: [
+            'HTML', 'DOM', 'JavaScript', 'Event Listeners'
+        ],
+        bgColorTop: `rgba(255, 251, 0, 0.4)`,
+        bgColorBottom: `rgba(255, 123, 0, 1)`,
+        image: `./images/mario.png`
+    }, {
+        Title: `Design a Blog`,
+        Description: `Design a responsive Blog page with a content feed`,
+        CourseLink: `http://coursework.vschool.io/design-a-blog/`,
+        LiveDemo: `./exercises/fromjon/blog/index.html`,
+        keyPoints: [
+            'HTML', 'CSS', 'Layout', "Box Model", "Media Queries"
+        ],
+        bgColorTop: `rgba(119, 153, 134, 0.3)`,
+        bgColorBottom: `rgba(135, 71, 51, 1)`,
+        image: `./images/blog.png`
+    }, {
+        Title: `CSS Flags`,
+        Description: `Build the following flags in pure HTML and CSS, no images allowed.`,
+        CourseLink: `https://coursework.vschool.io/css-flags/`,
+        LiveDemo: `./exercises/flags/index.html`,
+        keyPoints: [
+            'HTML', 'CSS', 'Layout', "Box Model"
+        ],
+        bgColorTop: `rgba(119, 153, 134, 0.3)`,
+        bgColorBottom: `rgba(135, 71, 51, 1)`,
+        image: `./images/flags2.png`
+    }, {
+        Title: `Simple Calculator`,
+        Description: `create a simple calculator that can Add, Subtract, and Multiply`,
+        CourseLink: `https://coursework.vschool.io/simple-calculator/`,
+        LiveDemo: `./exercises/simpleCalc/index.html`,
+        keyPoints: [
+            'HTML', 'CSS', "DOM", "Event Listeners", "Forms"
+        ],
+        bgColorTop: `rgba(119, 153, 134, 0.3)`,
+        bgColorBottom: `rgba(135, 71, 51, 1)`,
+        image: `./images/simpleCalc.png`
+    }
 ]
 
-
-
-deck = data.map(d=>`
+deck = data.map(d => `
 <div class="demo-card-square mdl-card mdl-shadow--2dp">
 <div class="mdl-card__title mdl-card--expand" style="background: linear-gradient(${d.bgColorTop}, ${d.bgColorBottom}),top left / cover no-repeat url(${d.image})">
 <h2 class="mdl-card__title-text">${d.Title}</h2></div>
 <div class="mdl-card__supporting-text">
     <p>${d.Description}</p>
-    <p>${d.keyPoints.map(e =>`<button type="button" class="mdl-chip"><span class="mdl-chip__text">${e}</span></button>`).join("\n")}</p>
+    <p>${d.keyPoints.map(e => `<button type="button" class="mdl-chip"><span class="mdl-chip__text">${e}</span></button>`).join("\n")}</p>
 </div>
 <div class="mdl-card__actions mdl-card--border">
     <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
